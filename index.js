cb9432f86048873b189a236b3ef6db196c049a6f
Arasu Ramanan <102941390+arasuramanan@users.noreply.github.com>
2024-01-01 19:56:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,31 @@ window.onscroll = function() {
         header.classList.remove("navbarDark");
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Target the elements by their IDs
+    var titleElement = document.getElementById('hero-title');
+    var descElement = document.getElementById('hero-desc');
+
+    // Function to type the text with pauses
+    function typeWithPauses(element, words, index) {
+        if (index < words.length) {
+            element.textContent += words[index] + ' ';
+            index++;
+            setTimeout(function () {
+                typeWithPauses(element, words, index);
+            }, 500); // Adjust the pause time (in milliseconds)
+        }
+    }
+
+    // Call the typing function for each element
+    typeWithPauses(titleElement, ['Hi,', 'it\'s', 'me', 'Arasu', 'Ramanan'], 0);
+
+    // Pause before typing the second sentence
+    setTimeout(function () {
+        // Clear the content of the second element before typing
+        descElement.textContent = '';
+        // Call the typing function for the second element
+        typeWithPauses(descElement, ['I', 'am', 'a', 'professional', 'Full', 'Stack', 'Developer'], 0);
+    }, 2000); // Adjust the pause time before typing the second sentence (in milliseconds)
+});
